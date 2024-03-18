04c84ed2a49d4cdeb7f37d82238abfdf59fd7060
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-18 10:27:28 +0000

    Initial commit

---
+++ 1.4/Source/SubpersonaAI/DefModExtensions/DefModExt_ProgramCartridge.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace SubpersonaAI
+{
+    public class DefModExt_ProgramCartridge : DefModExtension
+    {
+        public List<SkillDef> skills = new List<SkillDef>();
+
+        public TraitDef trait = null;
+    }
+}
