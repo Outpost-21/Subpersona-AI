f1c5a83431086e1c814a8bf3cfa9fdfd95aff19b
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-03-25 16:14:49 +0000

    1.5 stuff

---
+++ 1.5/Source/SubpersonaAI/SubpersonaDefOf.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace SubpersonaAI
+{
+    [DefOf]
+    public static class SubpersonaDefOf
+    {
+        static SubpersonaDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(SubpersonaDefOf));
+        }
+
+        public static TraitDef SubAI_ShellStandard;
+
+        public static JobDef SubAI_GatherProgramItem;
+        public static JobDef SubAI_EnterReconstructor;
+        public static JobDef SubAI_CarryToReconstructor;
+
+        public static ThingDef SubAI_SubpersonaShell;
+
+        public static SoundDef SubAI_OvenDing;
+    }
+}
---
+++ 1.5/Source/SubpersonaAI/SubpersonaMod.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+using HarmonyLib;
+using System.Reflection;
+using System.IO;
+
+namespace SubpersonaAI
+{
+    public class SubpersonaMod : Mod
+    {
+        public static SubpersonaMod mod;
+
+        public static string VersionDir => Path.Combine(mod.Content.ModMetaData.RootDir.FullName, "Version.txt");
+
+        public static string CurrentVersion { get; private set; }
+
+        public SubpersonaMod(ModContentPack content) : base(content)
+        {
+            mod = this;
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+            Log.Message($":: Subpersona Shells :: {CurrentVersion} ::");
+
+            if (Prefs.DevMode)
+            {
+                File.WriteAllText(VersionDir, CurrentVersion);
+            }
+
+            new Harmony("Neronix17.SubpersonaShells.RimWorld").PatchAll();
+        }
+    }
+
+
+}
